bb3b09daa23ffeedb8d9a45bcefd98a540aedd54
ligalaz <95artm.mick@gmail.com>
2022-12-15 14:46:10 +0300

    feat:add design resources

---
+++ src/resources/gamebase/characters.js
@@ -0,0 +1,82 @@
+export default [
+    {
+        id: 1,
+        name: "Bulbasaur",
+        img: "./resources/characters/Bulbasaur.png",
+    },
+    {
+        id: 2,
+        name: "Charizard",
+        img: "./resources/characters/Charizard.png",
+    },
+    {
+        id: 3,
+        name: "Charmander",
+        img: "./resources/characters/Charmander.png",
+    },
+    {
+        id: 4,
+        name: "Ivy",
+        img: "./resources/characters/Ivy.png",
+    },
+    {
+        id: 5,
+        name: "Lapras",
+        img: "./resources/characters/Lapras.png",
+    },
+    {
+        id: 6,
+        name: "Magicarp",
+        img: "./resources/characters/Magicarp.png",
+    },
+    {
+        id: 7,
+        name: "Miaut",
+        img: "./resources/characters/Miaut.png",
+    },
+    {
+        id: 8,
+        name: "Miu",
+        img: "./resources/characters/Miu.png",
+    },
+    {
+        id: 9,
+        name: "Miutu",
+        img: "./resources/characters/Miutu.png",
+    },
+    {
+        id: 10,
+        name: "Mr_Mime",
+        img: "./resources/characters/Mr_Mime.png",
+    },
+    {
+        id: 11,
+        name: "Picachu",
+        img: "./resources/characters/Picachu.png",
+    },
+    {
+        id: 12,
+        name: "Psyduck",
+        img: "./resources/characters/Psyduck.png",
+    },
+    {
+        id: 13,
+        name: "Slowpok",
+        img: "./resources/characters/Slowpok.png",
+    },
+    {
+        id: 14,
+        name: "Snorlax",
+        img: "./resources/characters/Snorlax.png",
+    },
+    {
+        id: 15,
+        name: "Squirtl",
+        img: "./resources/characters/Squirtl.png",
+    },
+    {
+        id: 16,
+        name: "Venusaur",
+        img: "./resources/characters/Venusaur.png",
+    },
+];
