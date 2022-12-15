6149a7230bd19f97e9a4b54a0f75e5af2c5c772f
ligalaz <95artm.mick@gmail.com>
2022-12-15 21:58:47 +0300

    feat:add currents interfaces

---
+++ src/interfaces/enums.ts
@@ -0,0 +1,11 @@
+export enum Kit {
+    Minimum = 8,
+    Middle = 12,
+    Maximum = 16,
+}
+
+export enum Timer {
+    Minimum = 60,
+    Middle = 90,
+    Maximum = 120,
+}
---
+++ src/interfaces/interface.ts
@@ -0,0 +1,10 @@
+export interface ICard {
+    id: number;
+    name: string;
+    url: string;
+}
+
+export interface IResult {
+  name:string;
+  time:Date;
+}
